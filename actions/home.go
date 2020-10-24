313c0c0860956b8de8a42dd810ee2a04edaad3d5
sagudeloo <sagudeloo@eafit.edu.co>
2020-10-24 13:25:58 -0500

    Redirecting root path to experience path

--- actions/home.go
+++ actions/home.go
@@ -1,13 +1,11 @@
 package actions
 
 import (
-	"net/http"
-
 	"github.com/gobuffalo/buffalo"
 )
 
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
-	return c.Render(http.StatusOK, r.HTML("index.html"))
+	return c.Redirect(307, "experiencesPath()")
 }
